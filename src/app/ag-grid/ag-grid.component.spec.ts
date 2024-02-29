08190459e3b58c5444e05acbd36a7bed238cd002
Dineshkumar510 <dk6089567@gmail.com>
2024-03-01 01:39:01 +0530

    Updated unit testing

--- src/app/ag-grid/ag-grid.component.spec.ts
+++ src/app/ag-grid/ag-grid.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AgGridComponent } from './ag-grid.component';
+import { AgGridModule } from 'ag-grid-angular';
+import {YoutubeService} from '../youtube.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AgGridComponent', () => {
   let component: AgGridComponent;
@@ -8,7 +10,8 @@ describe('AgGridComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ AgGridComponent ]
+      imports: [ AgGridComponent, HttpClientModule ],
+      providers: [YoutubeService],
     })
     .compileComponents();
 
@@ -20,4 +23,10 @@ describe('AgGridComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render ag-Grid', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('ag-grid-angular')).toBeTruthy();
+  });
+
 });
